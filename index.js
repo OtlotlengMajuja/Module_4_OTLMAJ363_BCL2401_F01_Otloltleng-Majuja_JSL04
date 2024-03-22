e852eeae82ef8c48c5b61397138c3d862a90192c
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-03-22 10:51:52 +0200

    Completed Challenge 3

--- index.js
+++ index.js
@@ -33,7 +33,12 @@ console.log(message);
 // If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
 
 let heartRate = 95; // Current heart rate in bpm
-console.log(`'}`);
+
+if (heartRate < 100) {
+    console.log("Boost needed!");
+} else {
+    console.log("Energy levels are high!")
+}
 
 
 // #### Challenge 4: Temperature Suitability
